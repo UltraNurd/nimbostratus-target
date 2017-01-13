3b5f1c825d553599efabdd9860ade65e8e979d91
Ward <nicowar@amazon.com>
2017-01-13 12:41:38 -0800

    Use instance profile for SQS permissions

--- servers/django_frontend/vulnweb/vulnweb/broker.py
+++ servers/django_frontend/vulnweb/vulnweb/broker.py
@@ -1,6 +1,6 @@
 BROKER_TRANSPORT = 'sqs'
 BOTO_PROVIDER = 'boto-provider'
-BROKER_URL = '%s://%s:%s@' % (BROKER_TRANSPORT, BOTO_PROVIDER, None)
+BROKER_URL = 'sqs://'
 CELERY_IGNORE_RESULT = True
 BROKER_TRANSPORT_OPTIONS = {
                             'queue_name_prefix': 'nimbostratus-',
